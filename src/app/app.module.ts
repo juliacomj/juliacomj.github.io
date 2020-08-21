461a615fa9b3934fd92e9f59279d14bce1893ed8
Júlia Roldi <julialcantara.ra@gmail.com>
2020-08-20 22:21:53 -0300

    new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ExpirienceComponent } from './components/expirience/expirience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ExpirienceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/expirience/expirience.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExpirienceComponent } from './expirience.component';
+
+describe('ExpirienceComponent', () => {
+  let component: ExpirienceComponent;
+  let fixture: ComponentFixture<ExpirienceComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ExpirienceComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExpirienceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
